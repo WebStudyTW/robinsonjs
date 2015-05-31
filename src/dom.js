fac8a6461537d3832221da3a87932259404afd89
C1mone <sun84041@gmail.com>
2015-05-31 16:42:18 +0800

    ADD: class Node & ElementData in dom.js

---
+++ src/dom.js
@@ -0,0 +1,25 @@
+class Node{
+    constructor(children, nodeType){
+        this.children =  children;
+        this.nodeType = nodeType;
+    }
+
+    static getTextNode(data){
+        return new Node([],data);
+    }
+
+    static getElementNode(name, attrMap, children){
+        return new Node(children, new ElementData(name, attrMap));
+    }
+}
+class ElementData{
+    constructor(tagName, attributes){
+        this.tagName = tagName;
+        this.attributes = attributes;
+    }
+}
+
+
+
+
+export default {Node};
